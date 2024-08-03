8a4c08ee5829370f1280872a129f31dd732c79db
Luke Yamasaki <lukeyamasaki@Lukes-MBP.lan>
2024-08-03 16:13:36 -0400

    add visual workshop links and update readme

---
+++ documentation/src/constants/visual-workshop-links.js
@@ -0,0 +1,9 @@
+export const hydraLinks = {header: 'Hydra links', links: [
+    {title: 'Interactive Documentation', url:'https://hydra.ojack.xyz/docs/', description: "Hydra's Interactive Documentation."},
+    {title: 'Functions list', url: 'https://hydra.ojack.xyz/api/', description: "Hydra's functions list."},
+]};
+
+export const liveCodeNYCLinks = {header: 'LivecodeNYC', links: [
+    {title: 'LivecodeNYC homepage', url:'https://livecode.nyc/', description: "LivecodeNYC's homepage."},
+    {title: 'LivecodeNYC', url: 'https://discord.gg/fcrAKbg', description: "LivecodeNYC's Discord channel."},
+]};
